85ec3490df1cb8f359c59ee511520670698800d3
Bobby Khounphinith <khoupe07@gmail.com>
2019-04-15 08:49:01 -0500

    created a function to delete employee info by matching id info

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let employeeInfo = [];
 
 function readyNow() {
     $('#addInfoButton').on('click', addInfo);
-    displayInfo(employeeInfo);
-    
+    $('#deleteButton').on('click', deleteEmployee);
+    displayInfo(employeeInfo);   
 }
 
 function addInfo() { // a function to add employee's info.
     console.log('in addInfo:');
-    const newInfo = { // new object to store info from inputs
+    const newInfo = { // new object to store emloyee's infos.
         firstName: $('#firstName').val(),
         lastName: $('#lastName').val(),
         idInfo: $('#idInfo').val(),
@@ -20,6 +20,8 @@ function addInfo() { // a function to add employee's info.
     employeeInfo.push(newInfo); // stores newInfo into employeeInfo array
     displayInfo(employeeInfo); // employee info will display on DOM
     totalMonthly();
+
+
     // Deletes input fields after infos are submitted
     $('#firstName').val('');
     $('#lastName').val('');
@@ -30,12 +32,14 @@ function addInfo() { // a function to add employee's info.
 
 
 
+
 function displayInfo() { // a function to display employee's info
     console.log('in displayInfo');
     let el = $('#infoOut');
     el.empty();
     for(let i = 0; i < employeeInfo.length; i++) {
-        const listInfo = `<tr>
+        // displaying infos to the DOM
+        const listInfo = `<tr> 
         <td>${employeeInfo[i].firstName}</td>
         <td>${employeeInfo[i].lastName}</td>
         <td>${employeeInfo[i].idInfo}</td>
@@ -52,10 +56,46 @@ function totalMonthly() { // a function to calculate the total monthly.
         total = total + (employeeInfo[i].annualSalary/12); // Employee's annual salary divided by 12 months.
         $('.showTotalMonthly').text(`Total Monthly: $${total.toFixed(2)}`);
     } // for loop.
+    // if total monthly is over 20,000, then background will turn red
     if(total > maxSalary) {
         $('.showTotalMonthly').css('background-color', 'red');
     } // end if
+    //if total monthly is under 20,000, then background will not change.
+    else {
+        $('.showTotalMonthly').css('background-color', 'lightcyan');
+
+    } //end else.
 } // end totalMonthly.
+
+
+// a function to delete employee by matching the id info.
+function deleteEmployee() {
+    console.log('in deleteEmployee');
+    
+    const employeeObject = {
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        idInfo: $('#idInfo').val(),
+        titleName: $('#titleName').val(),
+        annualSalary: $('#annualSalary').val()
+    }
+
+    for(i=0; i<employeeInfo.length; i++) {
+        if(employeeInfo[i].idInfo == employeeObject.idInfo) {
+                employeeInfo.splice(i, 1);
+            } // end if 
+    } //end for loop
+    
+    // clear inputs
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#idInfo').val('');
+    $('#titleName').val('');
+    $('#annualSalary').val('');
+
+    addInfo();
+    
+} // end deleteEmployee
 
 
 
